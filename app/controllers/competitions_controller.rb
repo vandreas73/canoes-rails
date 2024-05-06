b016a0b0a1f968d1fa7bb9b8cc292a01879a54e2
vandreas73 <v.andras2001@gmail.com>
2024-05-06 15:02:48 +0200

    competitions edit, new works

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -8,6 +8,23 @@ class CompetitionsController < ApplicationController
   end
 
   def edit
-    @competition = Competition.new(name: "WC", date: '2022-06-14')
+    @competition = Competition.find(params[:id])
   end
+
+  def update
+    @competition = Competition.find(params[:id])
+    @competition.update(competition_params)
+    redirect_to competition_index_path(@competition.canoe_id)
+  end
+
+  def create
+    @competition = Competition.new(competition_params)
+    @competition.save
+    redirect_to competition_index_path(params[:canoe_id])
+  end
+
+  private
+    def competition_params
+      params.require(:competition).permit(:name, :date, :canoe_id)
+    end
 end
